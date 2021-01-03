c99feb640ca4ccf871b57c3bfd498ac1433a47a4
Isshana <isshana.mohanakumar@gmail.com>
2021-01-03 12:43:24 -0500

    First commit

---
+++ blog/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+class Category(models.Model):
+  name = models.CharField(max_length=225)
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('home')  
+
+class Post(models.Model):
+  title = models.CharField(max_length=100)
+  content = models.TextField()
+  date_posted = models.DateTimeField(default=timezone.now)
+  author = models.ForeignKey(User, on_delete=models.CASCADE) 
+  category = models.CharField(max_length=225,default='general')
+
+
+  def __str__(self):
+    return self.title
+
+  def get_absolute_url(self):
+    return reverse('post-detail', kwargs={'pk':self.pk})  
+
+
---
+++ blog/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+from .views import (
+    PostListView, 
+    PostDetailView, 
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    UserPostListView
+)    
+from . import views
+
+urlpatterns = [
+    path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('fitness/',views.fitness,name="blog-fitness"),
+    path('mentalhealth/',views.mentalhealth,name="blog-mentalhealth"),
+    path('about/', views.about, name='blog-about'),
+    path('calendar/',views.calendar,name="blog-calendar"),
+    path('chat/',views.chat,name="blog-chat")
+]
+
